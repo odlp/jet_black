20efc04cfdead8e3117cf2b4cdc43537f1dc9e71
Oli Peate <oli@thoughtbot.com>
2018-06-22 13:08:22 +0100

    Add raw_stdout and raw_stderr attributes
    Provides access to original unedited stdout and stderr from a process

--- lib/jet_black/executed_command.rb
+++ lib/jet_black/executed_command.rb
@@ -2,10 +2,13 @@ require_relative "ansi_scrubber"
 
 module JetBlack
   class ExecutedCommand
-    attr_reader :raw_command, :stdout, :stderr, :exit_status
+    attr_reader :raw_command, :raw_stdout, :raw_stderr,
+                :stdout, :stderr, :exit_status
 
     def initialize(raw_command:, stdout:, stderr:, exit_status:)
       @raw_command = raw_command
+      @raw_stdout = stdout
+      @raw_stderr = stderr
       @stdout = AnsiScrubber.call(stdout.chomp)
       @stderr = AnsiScrubber.call(stderr.chomp)
       @exit_status = exit_status.to_i
