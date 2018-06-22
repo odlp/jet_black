3953bae8f1bdff0c14849913b971d8eda077d3b3
Oli Peate <oli@thoughtbot.com>
2018-06-22 13:08:22 +0100

    Add raw_stdout and raw_stderr attributes
    Provides access to original unedited stdout and stderr from a process

--- spec/features/run_ansi_scrubbing_spec.rb
+++ spec/features/run_ansi_scrubbing_spec.rb
@@ -3,27 +3,46 @@ require "jet_black"
 RSpec.describe JetBlack::Session, "ANSI scrubbing" do
   describe "stdout" do
     it "scrubs ANSI color escape sequences" do
-      subject.create_executable "print-green", <<~SH
-        #!/bin/sh
-        printf "\033[1;32mThis is green text\033[0m\n"
-      SH
+      create_printer("print-green", '\033[1;32mThis is green text\033[0m\n')
 
       result = subject.run("./print-green")
 
       expect(result.stdout).to eq "This is green text"
     end
+
+    it "preserves the original output in #raw_stdout" do
+      create_printer("print-green", '\033[1;32mThis is green text\033[0m\n')
+
+      result = subject.run("./print-green")
+
+      expect(result.raw_stdout).to eq "\e[1;32mThis is green text\e[0m\n"
+    end
   end
 
   describe "stderr" do
     it "scrubs ANSI color escape sequences" do
-      subject.create_executable "print-red", <<~SH
-        #!/bin/sh
-        printf "\033[1;31mThis is red text\033[0m\n"
-      SH
+      create_printer("print-red", '\033[1;31mThis is red text\033[0m\n')
 
       result = subject.run("./print-red 1>&2")
 
       expect(result.stderr).to eq "This is red text"
     end
+
+    it "preserves the original output in #raw_stderr" do
+      create_printer("print-red", '\033[1;31mThis is red text\033[0m\n')
+
+      result = subject.run("./print-red 1>&2")
+
+      expect(result.raw_stderr).to eq "\e[1;31mThis is red text\e[0m\n"
+    end
+  end
+
+  private
+
+  def create_printer(printer_name, string)
+    subject.create_executable printer_name, <<~SH
+      #!/bin/sh
+      printf "#{string}"
+    SH
   end
 end
