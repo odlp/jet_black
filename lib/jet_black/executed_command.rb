889e36faccc48b65965d7d87c7a788530dd5ba57
Oli Peate <oli@thoughtbot.com>
2018-06-22 11:46:35 +0100

    Scrub ANSI escape sequences

--- lib/jet_black/executed_command.rb
+++ lib/jet_black/executed_command.rb
@@ -1,11 +1,13 @@
+require_relative "ansi_scrubber"
+
 module JetBlack
   class ExecutedCommand
     attr_reader :raw_command, :stdout, :stderr, :exit_status
 
     def initialize(raw_command:, stdout:, stderr:, exit_status:)
       @raw_command = raw_command
-      @stdout = stdout.chomp
-      @stderr = stderr.chomp
+      @stdout = AnsiScrubber.call(stdout.chomp)
+      @stderr = AnsiScrubber.call(stderr.chomp)
       @exit_status = exit_status.to_i
     end
 
---
+++ spec/lib/jet_black/ansi_scrubber_spec.rb
@@ -0,0 +1,35 @@
+require "jet_black/ansi_scrubber"
+
+RSpec.describe JetBlack::AnsiScrubber do
+  subject { described_class }
+
+  it "scrubs colors" do
+    string = "This is \e[33mGreen\e[0m"
+
+    expect(subject.call(string)).to eq "This is Green"
+  end
+
+  it "scrubs colors from multi-line text" do
+    string = "This is\n\e[33mGreen\e[25m"
+
+    expect(subject.call(string)).to eq "This is\nGreen"
+  end
+
+  it "scrubs escapes" do
+    string = "Contains \e~ an escape ending in tilde"
+
+    expect(subject.call(string)).to eq "Contains  an escape ending in tilde"
+  end
+
+  it "scrubs escapes with intermediate characters" do
+    string = "Contains \e$~ an escape ending in tilde"
+
+    expect(subject.call(string)).to eq "Contains  an escape ending in tilde"
+  end
+
+  it "scrubs escapes with non-ASCII characters" do
+    string = "ジェット\e[33m黒"
+
+    expect(subject.call(string)).to eq "ジェット黒"
+  end
+end
